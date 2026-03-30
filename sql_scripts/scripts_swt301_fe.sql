5a0fab7e7b6b79e63373d694198b67af28d7da4c
x1ptase <anhtph911@gmail.com>
2026-03-31 00:05:22 +0700

    docs: update db for swt301

---
+++ sql_scripts/scripts_swt301_fe.sql
@@ -0,0 +1,12 @@
+﻿USE quiz_db;
+GO
+
+-- 2. Xóa sạch dữ liệu cũ để tránh lỗi ID hoặc lỗi logic
+TRUNCATE TABLE UserResponses;
+TRUNCATE TABLE QuizQuestions;
+GO
+
+SELECT * FROM QuizQuestions;
+
+INSERT INTO QuizQuestions (question, option_a, option_b, option_c, option_d, correct_answer) VALUES 
+(N'Beta testing is performed at developing organizations site where as Alpha testing is performed by people at their own locations', N'True', N'False', '', '', 'B');
